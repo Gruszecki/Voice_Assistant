ec7c88868bbbfcf64358322391db5265c24e84ff
Gruszecki <wojtek.gruszecki@gmail.com>
2022-09-16 17:05:31 +0200

    Feature: open app

--- commands.py
+++ commands.py
@@ -1,7 +1,8 @@
-import subprocess
 import datetime
+import subprocess
+from configparser import ConfigParser
 
-import communication
+from communication import speak, get_audio
 
 WAKE = 'janko'
 
@@ -11,51 +12,56 @@ commands_list = {
         'jaki mamy czas',
         'która jest',
         'jaki jest czas'
+    ],
+    'open_app': [
+        'otwórz aplikację'
     ]
 }
 
+config = ConfigParser()
+config.read('config.ini')
+
 
 def greetings():
-    # communication.speak(f'Nazywam się Yanka. Jestem asystentem głosowym. Jeśli będziesz mnie potrzebował, zawołaj mnie: {WAKE}')
-    communication.speak(f'Zawołaj mnie: {WAKE}')
+    speak(f'Nazywam się Yanka. Jestem asystentem głosowym. Jeśli będziesz mnie potrzebował, zawołaj mnie: {WAKE}')
+    # speak(f'Zawołaj mnie: {WAKE}')
 
 def validate_text(text):
     match text:
         case 'NOT UNDERSTOOD':
-            communication.speak('Nie zrozumiałam.')
+            speak('Nie zrozumiałam.')
             return 0
         case 'ERROR':
-            communication.speak('Wystąpił błąd. Nie wiem co się dzieje.')
+            speak('Wystąpił błąd. Nie wiem co się dzieje.')
             return 0
         case _:
             return text
 
 def listen():
-    text = communication.get_audio()
-    print(text)
+    text = get_audio()
+
     if text.count(WAKE) > 0:
-        communication.speak('Tak?')
-        text = validate_text(communication.get_audio())
+        speak('Tak?')
+        text = validate_text(get_audio())
 
         if text:
             if 'wyłącz się' in text:
-                communication.speak('Żegnam ozięble.')
+                speak('Żegnam ozięble.')
                 return 0
             else:
                 result = execute_command(text)
 
                 if not result:
-                    communication.speak(f'Nie znalazłam akcji dla: {text}')
+                    speak(f'Nie znalazłam akcji dla: {text}')
 
     return 1
 
 def execute_command(text):
-    print('Szukam', text)
     for key, value in commands_list.items():
-        print(key, value)
-        if text in value:
-            exec(f'{key}()')
-            return 1
+        for v in value:
+            if v in text:
+                exec(f'{key}()')
+                return 1
 
     return 0
 
@@ -68,7 +74,7 @@ def say_time():
 
     match hour:
         case 0:
-            text += f'{time_now[1]} minut po północy'
+            text += 'zero '
         case 4 | 5 | 6 | 9 | 10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18 | 19 | 20:
             text += f'{hour}-ta '
         case 1 | 21:
@@ -80,7 +86,17 @@ def say_time():
         case 7 | 8:
             text += f'{hour}-ma '
 
-    if hour:
-        text += minutes
+    text += minutes
 
-    communication.speak(text)
+    speak(text)
+
+def open_app():
+    speak('Jaką aplikację mam otworzyć?')
+    text = validate_text(get_audio())
+
+    if text:
+        try:
+            app_path = config['apps'][text]
+            subprocess.Popen(app_path)
+        except KeyError:
+            speak(f'Nie odnaleziono adresu aplikacji {text} w pliku konfiguracyjnym.')
