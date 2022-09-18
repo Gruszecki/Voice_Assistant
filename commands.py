8c688451d795ced92fbd137a3602e5ef2d4697ca
Gruszecki <wojtek.gruszecki@gmail.com>
2022-09-18 16:14:48 +0200

    Feature: push button Feature: click on screen

--- commands.py
+++ commands.py
@@ -1,8 +1,15 @@
 import datetime
+import easyocr
+import mss
+import numpy as np
+import pynput
 import subprocess
+
 from configparser import ConfigParser
+from PIL import Image, ImageDraw
 
 from communication import speak, get_audio
+
 
 WAKE = 'janko'
 
@@ -14,16 +21,28 @@ commands_list = {
         'jaki jest czas'
     ],
     'open_app': [
-        'otwórz aplikację'
+        'otwórz aplikację',
+        'otwórz program',
+        'włącz aplikację',
+        'włącz program'
+    ],
+    'press_key': [
+        'wciśnij klawisz'
+    ],
+    'click_on_screen': [
+        'kliknij'
     ]
 }
 
 config = ConfigParser()
 config.read('config.ini')
 
+keyboard = pynput.keyboard.Controller()
+mouse = pynput.mouse.Controller()
+
 
 def greetings():
-    speak(f'Nazywam się Yanka. Jestem asystentem głosowym. Jeśli będziesz mnie potrzebował, zawołaj mnie: {WAKE}')
+    speak(f'Nazywam się Janka. Jestem asystentem głosowym. Jeśli będziesz mnie potrzebował, zawołaj mnie: {WAKE}')
     # speak(f'Zawołaj mnie: {WAKE}')
 
 def validate_text(text):
@@ -100,3 +119,49 @@ def open_app():
             subprocess.Popen(app_path)
         except KeyError:
             speak(f'Nie odnaleziono adresu aplikacji {text} w pliku konfiguracyjnym.')
+
+def press_key():
+    speak('Jaki klawisz mam wcisnąć?')
+    text = validate_text(get_audio())
+
+    if text:
+        if len(text) == 1 and text.isalnum():
+            try:
+                keyboard.press(text)
+                keyboard.release(text)
+            except AttributeError:
+                speak(f'Nie mogę wcisnąć klawisza {text}')
+        else:
+            match text:
+                case 'start':
+                    text = 'cmd'
+
+            try:
+                exec(f'keyboard.press(pynput.keyboard.Key.{text})')
+                exec(f'keyboard.release(pynput.keyboard.Key.{text})')
+            except AttributeError:
+                speak(f'Nie mogę wcisnąć klawisza {text}')
+
+def click_on_screen():
+    speak('Co mam kliknąć?')
+    voice_text = validate_text(get_audio())
+
+    if voice_text:
+        speak(f'Szukam {voice_text}')
+        with mss.mss() as sct:
+            screenshot = sct.shot(mon=1, output='last_screenshot.png')
+
+            reader = easyocr.Reader(['pl', 'en'])
+            detected = reader.detect(screenshot, width_ths=0.7, mag_ratio=1.5)
+            text_coordinates = detected[0][0]
+            recognized = reader.recognize(screenshot, horizontal_list=text_coordinates, free_list=[])
+            border_coordinates = [[txt[0][0], txt[0][2]] for txt in recognized if txt[1].lower() == voice_text]
+
+            if len(border_coordinates):
+                target_x = int((border_coordinates[0][0][0] + border_coordinates[0][1][0]) / 2)
+                target_y = int((border_coordinates[0][0][1] + border_coordinates[0][1][1]) / 2)
+
+                mouse.position = (target_x, target_y)
+                mouse.click(pynput.mouse.Button.left, count=1)
+            else:
+                speak(f'Nie znalazłam {voice_text}')
